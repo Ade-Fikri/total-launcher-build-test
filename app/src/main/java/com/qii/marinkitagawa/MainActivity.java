57e470d408276543f6273f27206b3fb066b0d2a2
Ade-Fikri <aadefikrii@gmail.com>
2026-09-03 16:39:30 +0700

    Fix fullscreen layout and preview frame overlay

--- app/src/main/java/com/qii/marinkitagawa/MainActivity.java
+++ app/src/main/java/com/qii/marinkitagawa/MainActivity.java
@@ -46,7 +46,12 @@ public class MainActivity extends Activity {
                   android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN,
                   android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
           );
-      
+          
+          getWindow().setFlags(
+                  android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                  android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+          );
+          
           getWindow().getDecorView().setSystemUiVisibility(
                   View.SYSTEM_UI_FLAG_FULLSCREEN
                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
