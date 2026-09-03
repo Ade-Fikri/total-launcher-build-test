0308ff49bc40e5a5b06dba18f4dc9301b8841bda
Ade-Fikri <aadefikrii@gmail.com>
2026-09-03 14:43:00 +0700

    Redesign Marin Kitagawa theme app UI

--- app/src/main/java/com/qii/marinkitagawa/MainActivity.java
+++ app/src/main/java/com/qii/marinkitagawa/MainActivity.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2015 ChYK
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.qii.marinkitagawa;
 
 import android.app.Activity;
@@ -25,95 +25,136 @@ import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	private static final String pkgTotal = "com.ss.launcher2";
-	/**
-	 * lowest required version of Total Launcher
-	 */
-	private static final int minVersion = 10100;
+    private static final String pkgTotal = "com.ss.launcher2";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.activity_main);
-	}
+    /**
+     * lowest required version of Total Launcher
+     */
+    private static final int minVersion = 10100;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		this.getMenuInflater().inflate(R.menu.option_menu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.menuApply) {
-			boolean applicable;
-			try {
-				applicable = this.getPackageManager().getPackageInfo(pkgTotal, 0).versionCode >= minVersion;
-			} catch (NameNotFoundException e) {
-				applicable = false;
-			}
-			if (!applicable) {
-				ApplyThemeDlgFragment df = new ApplyThemeDlgFragment();
-				df.show(this.getFragmentManager(), "install_total_dlg");
-				return true;
-			}
-			Intent intent = new Intent(Constants.ACTION_APPLY_THEME);
-			intent.setComponent(Constants.COMPONENT_NAME);
-			intent.putExtra(Constants.EXTRA_THEME_PACKAGE, this.getPackageName());
-			intent.putExtra(Constants.EXTRA_THEME_ID, MyThemeProvider.THEME_IDS[0]);
-			startActivityForResult(intent, R.string.apply);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        Button applyButton = (Button) findViewById(R.id.applyButton);
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == R.string.apply && resultCode == Activity.RESULT_OK) {
-			// when applied the theme successfully
-			this.finish();
-			Intent intent = new Intent(Intent.ACTION_MAIN);
-			intent.addCategory(Intent.CATEGORY_LAUNCHER);
-			intent.setComponent(ComponentName.unflattenFromString("com.ss.launcher2/.MainActivity"));
-			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-			startActivity(intent);
-			return;
-		}
-		super.onActivityResult(requestCode, resultCode, data);
-	}
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyTheme();
+            }
+        });
+    }
 
-	public static Intent getMarketOpenIntent(String pname) {
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setData(Uri.parse("market://details?id="+pname));
-		return intent;
-	}
-	
-	///////////////////////////////////////////////////////////////////////////////
-	public static class ApplyThemeDlgFragment extends DialogFragment {
-		
-		@SuppressWarnings("deprecation")
-		public ApplyThemeDlgFragment() {}
-		
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
-			b.setTitle(R.string.not_installed).setMessage(R.string.install_total);
-			b.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					Intent intent = getMarketOpenIntent(pkgTotal);
-					startActivity(intent);
-				}
-			});
-			b.setNegativeButton(android.R.string.no, null);
-			return b.create();
-		}
-		
-	}
-	
+    private void applyTheme() {
+        boolean applicable;
+
+        try {
+            applicable = getPackageManager()
+                    .getPackageInfo(pkgTotal, 0)
+                    .versionCode >= minVersion;
+        } catch (NameNotFoundException e) {
+            applicable = false;
+        }
+
+        if (!applicable) {
+            ApplyThemeDlgFragment df = new ApplyThemeDlgFragment();
+            df.show(getFragmentManager(), "install_total_dlg");
+            return;
+        }
+
+        Intent intent = new Intent(Constants.ACTION_APPLY_THEME);
+        intent.setComponent(Constants.COMPONENT_NAME);
+        intent.putExtra(
+                Constants.EXTRA_THEME_PACKAGE,
+                getPackageName()
+        );
+        intent.putExtra(
+                Constants.EXTRA_THEME_ID,
+                MyThemeProvider.THEME_IDS[0]
+        );
+
+        startActivityForResult(intent, R.string.apply);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        if (requestCode == R.string.apply
+                && resultCode == Activity.RESULT_OK) {
+
+            // when applied the theme successfully
+            finish();
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setComponent(
+                    ComponentName.unflattenFromString(
+                            "com.ss.launcher2/.MainActivity"
+                    )
+            );
+            intent.setFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+            );
+
+            startActivity(intent);
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static Intent getMarketOpenIntent(String pname) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=" + pname));
+        return intent;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    public static class ApplyThemeDlgFragment extends DialogFragment {
+
+        @SuppressWarnings("deprecation")
+        public ApplyThemeDlgFragment() {}
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
+
+            b.setTitle(R.string.not_installed)
+                    .setMessage(R.string.install_total);
+
+            b.setPositiveButton(
+                    android.R.string.yes,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(
+                                DialogInterface dialog,
+                                int which) {
+
+                            Intent intent =
+                                    getMarketOpenIntent(pkgTotal);
+
+                            startActivity(intent);
+                        }
+                    }
+            );
+
+            b.setNegativeButton(
+                    android.R.string.no,
+                    null
+            );
+
+            return b.create();
+        }
+    }
 }
