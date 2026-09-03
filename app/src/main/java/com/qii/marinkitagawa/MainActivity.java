b377f7414139471e3c171cda9b665ac61b981169
Ade-Fikri <aadefikrii@gmail.com>
2026-09-03 15:53:12 +0700

    Enable immersive fullscreen preview

--- app/src/main/java/com/qii/marinkitagawa/MainActivity.java
+++ app/src/main/java/com/qii/marinkitagawa/MainActivity.java
@@ -37,26 +37,29 @@ public class MainActivity extends Activity {
      */
     private static final int minVersion = 10100;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    
-        getWindow().setStatusBarColor(
-                getResources().getColor(R.color.theme_background)
-        );
-    
-        getWindow().getDecorView().setSystemUiVisibility(0);
-    
-        Button applyButton = (Button) findViewById(R.id.applyButton);
-    
-        applyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                applyTheme();
-            }
-        });
-    }
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+      
+          getWindow().getDecorView().setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                  | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          );
+      
+          Button applyButton = (Button) findViewById(R.id.applyButton);
+      
+          applyButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  applyTheme();
+              }
+          });
+      }
 
     private void applyTheme() {
         boolean applicable;
